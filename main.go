2e461755a2ea82f6713109dacea4ba3a01c479b0
empijei <empijei@gmail.com>
2017-05-31 14:05:04 +0200

    Increased logging verbosity

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"crypto/tls"
+	"log"
 	"os"
 	"os/signal"
 )
@@ -9,13 +11,26 @@ var rules []listener
 
 func main() {
 
-	//TODO parse parameters and create rules
+	//TODO parse CLI parameters and create rules
+
+	//This is the creation of a sample rule
+	cer, err := tls.LoadX509KeyPair("server.crt", "server.key")
+	if err != nil {
+		log.Println(err)
+		return
+	}
 
 	rules = append(rules, listener{
 		localport:  ":9443",
 		remoteport: ":8000",
 		remoteip:   "127.0.0.1",
+		secure:     true,
+		certconf: &tls.Config{
+			Certificates:       []tls.Certificate{cer},
+			InsecureSkipVerify: true,
+		},
 	})
+	//Rule creation ends here
 
 	for _, l := range rules {
 		l := l
--- proxy.go
+++ proxy.go
@@ -14,32 +14,46 @@ type listener struct {
 	certconf                        *tls.Config
 }
 
+func (l *listener) String() string {
+	var text = "encrypted"
+	//This is ugly but fun
+	if !l.secure {
+		text = "un" + text
+	}
+	return l.localport + " -> " + l.remoteip + l.remoteport + " " + text
+}
+
 func (l *listener) Listen() {
 	//TODO check if the listener was properly constructed
 
 	var ll net.Listener
 	var err error
 	if l.secure {
-		//TODO setup conf
-		ll, err = tls.Listen("tcp", l.localport, nil)
+		ll, err = tls.Listen("tcp", l.localport, l.certconf)
 	} else {
 		ll, err = net.Listen("tcp", l.localport)
 	}
 	if err != nil {
 		log.Fatal(err)
 	}
+	log.Println("Running rule " + l.String())
 	for {
 		conn, err := ll.Accept()
 		if err != nil {
 			log.Fatal(err)
 		}
 		go func(lc net.Conn) {
+			log.Printf("Got incoming connection from ip %s on port %s", conn.RemoteAddr().String(), l.localport)
 			defer func() { _ = lc.Close() }()
 			//Dump traffic to stdout
 			lt := io.TeeReader(lc, os.Stdout)
 			var rc net.Conn
 			if l.secure {
-				rc, err = tls.Dial("tcp", l.remoteip+l.remoteport, nil)
+				rc, err = tls.Dial("tcp", l.remoteip+l.remoteport,
+					&tls.Config{
+						//TODO make this configurable
+						InsecureSkipVerify: true,
+					})
 			} else {
 				rc, err = net.Dial("tcp", l.remoteip+l.remoteport)
 			}
